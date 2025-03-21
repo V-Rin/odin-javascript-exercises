e762d301c955beddb7402702e0b4f9084bca7024
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-21 18:25:10 +0300

    Added solution for exercise 3, all tests were passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    let stringChars = stringInput.split("");
+    let result = "";
+    
+    for(let i = stringChars.length - 1; i >= 0; i--){
+        result += stringChars[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
