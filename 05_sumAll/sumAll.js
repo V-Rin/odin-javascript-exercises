7f211833c199281c7c3ae4ddf858aa9d817c0bb0
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-21 20:19:42 +0300

    Added solution for exercise 5, all tests were passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum){
+    if((typeof startNum !== "number")||(typeof endNum !== "number")
+        ||(!Number.isInteger(startNum))||(!Number.isInteger(endNum))
+            ||(startNum < 0)||(endNum < 0)){
+                return "ERROR";
+    }
 
+    let start;
+    let end;
+    let sum = 0;
+
+    if(startNum >= endNum){
+        start = endNum;
+        end = startNum;
+    }else{
+        start = startNum;
+        end = endNum;
+    }
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
