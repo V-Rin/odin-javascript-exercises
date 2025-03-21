708a979ad813f2c33ca072a05eaf3684f0fd02a6
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-21 15:01:04 +0300

    Added solution for exercise 2 with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputString, numOfRepeats) {
+    let result = "";
 
+    if(numOfRepeats < 0){
+        result = "ERROR";
+        return result;
+    }
+
+    for (let i = 0; i < numOfRepeats; i++){
+        result += inputString;
+    }
+    return result;
 };
 
 // Do not edit below this line
