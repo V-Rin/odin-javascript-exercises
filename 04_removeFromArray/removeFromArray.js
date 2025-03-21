dd24b5e6d5b90f22c0e3f4a04b51b256b510c953
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-21 19:12:05 +0300

    Added solution for exercise 4, all tests were passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...elementsToBeRemoved) {
+    const result = originalArray.filter((element)=>{
+        let notEqual = true;
+        for(let removable of elementsToBeRemoved){
+            if(removable === element){
+                notEqual = false;
+                break;
+            }
+        }
+        return notEqual;
+    })
+    return result;
 };
 
 // Do not edit below this line
