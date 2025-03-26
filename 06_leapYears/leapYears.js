1331c13173dbb23f825e69b20697a004f2dd63d2
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-26 19:09:59 +0300

    Solution for exercise 6 added, all tests were passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if((yearInput%4 == 0)&&((yearInput%100 == 0)&&(yearInput%400 == 0))||(yearInput%4 == 0)&&(yearInput%100 != 0)) return true;
+    else return false;
 };
 
 // Do not edit below this line
