d499d12e560259b085a6cba6ba2353d45e1bd698
V-Rin <79162610+V-Rin@users.noreply.github.com>
2025-03-26 19:40:09 +0300

    Solution for exercise 7 added, all tests were passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInput) { //Temperature input measurement is Fahrenheit
+  let result = (temperatureInput - 32) / 1.8;
+
+  if(Number.isInteger(result)) return result;
+  else return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInput) { //Temperature input measurement is Celsus
+  let result = temperatureInput * 1.8 + 32;
+
+  if(Number.isInteger(result)) return result;
+  else return +result.toFixed(1);
 };
 
 // Do not edit below this line
